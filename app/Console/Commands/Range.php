b66a32774657e57881266fb6d72c7b56c5ea17e1
Carterror <carlos.bramila98@gmail.com>
2021-08-13 17:44:46 -0400

    Update Range.php

--- app/Console/Commands/Range.php
+++ app/Console/Commands/Range.php
@@ -43,7 +43,7 @@ class Range extends Command
         $users = User::where('rango', '<', 20)->get();
 
         foreach ($users as $user) {
-        Storage::append("range.log", $user);
+        Storage::append("range.log", "asdasda");
             $count = 0;
             $refers = User::where('master', $user->id)->get();
             foreach ($refers as $refer) {
