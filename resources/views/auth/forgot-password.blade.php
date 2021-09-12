eb480c0c374a5a20dd79cabc432b664d84cb66e8
Carterror <carlos.bramila98@gmail.com>
2021-09-12 13:16:43 -0400

    reset password
    vistas de reseteo de pass

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -6,31 +6,31 @@
             </a>
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm" style="color: beige; margin-bottom: 20px; padding: 10px; font-size: large;">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <x-auth-session-status class="mb-4" :status="session('status')" style="color: beige; padding: 10px;"/>
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.email') }}">
+    <div class="row" style="margin-top: 20px; padding-left: 15px;">
+        <form method="POST" action="{{ route('password.email') }}" >
             @csrf
 
             <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="input-field col s11 blue-grey-text text-lighten-5">
+                <i class="mdi-content-drafts prefix"></i>
+                <input id="email" class="blue-grey-text text-lighten-5" type="email" name="email" value="{{old('email')}}" required autofocus>
+                <label for="email">Correo Electrónico</label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
+            <x-button style="margin: 10px;">
+                {{ __('Enviar Correo') }}
+            </x-button>
         </form>
+    </div>
     </x-auth-card>
 </x-guest-layout>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -9,40 +9,39 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.update') }}">
+        <form method="POST" action="{{ route('password.update') }}" style="margin-top: 20px; padding-left: 15px;">
             @csrf
 
             <!-- Password Reset Token -->
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
             <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <div class="input-field col s11 blue-grey-text text-lighten-5">
+                <i class="mdi-content-drafts prefix"></i>
+                <input id="email" class="blue-grey-text text-lighten-5" type="email" name="email" value="{{old('email')}}" required autofocus>
+                <label for="email">Correo Electrónico</label>
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <div class="row">
+                <div class="input-field col s11 blue-grey-text text-lighten-5">
+                    <a onclick="verpass('password')" class="prefix"><i class="mdi-image-remove-red-eye tooltipped" id="password icono" data-position="top" data-delay="50" data-tooltip="Ver Contraseña"></i></a>
+                    <input class="blue-grey-text text-lighten-5" id="password" type="password" name="password" required autocomplete="current-password">
+                    <label for="password">Contraseña</label>
+                </div>
+              </div>
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
+              <div class="row">
+                <div class="input-field col s11 blue-grey-text text-lighten-5">
+                    <a onclick="verpass('password_confirmation')" class="prefix"><i class="mdi-image-remove-red-eye tooltipped" id="password_confirmation icono" data-position="top" data-delay="50" data-tooltip="Ver Contraseña"></i></a>
+                    <input class="blue-grey-text text-lighten-5" id="password_confirmation" type="password" name="password_confirmation" required>
+                    <label for="password_confirmation">Confirmar Contraseña</label>
+                </div>
+              </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
+            <x-button style="margin: 10px;">
+                {{ __('Reset Password') }}
+            </x-button>
         </form>
     </x-auth-card>
 </x-guest-layout>
