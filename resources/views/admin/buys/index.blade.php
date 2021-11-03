cfdec075be0f9b95eb3077e62d9b713123264e59
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2021-11-03 14:21:13 -0400

    Update index.blade.php
    fecha rectificada

--- resources/views/admin/buys/index.blade.php
+++ resources/views/admin/buys/index.blade.php
@@ -73,9 +73,9 @@ Compras
                                     @if ($buy->estado == 1)
                                         <div class="card-panel blue lighten-2" style="padding: 5px; color: #fff;">{{$buy->updated_at->format('d/m/Y')}}</div>
                                     @elseif ($buy->estado == 2)
-                                        <div class="card-panel teal lighten-2" style="padding: 5px; color: #fff;">{{$buy->updated_at->format('Y/m/d')}}</div>
+                                        <div class="card-panel teal lighten-2" style="padding: 5px; color: #fff;">{{$buy->updated_at->format('d/m/Y')}}</div>
                                     @else
-                                        <div class="card-panel red lighten-2" style="padding: 5px; color: #fff;">{{$buy->updated_at->format('Y/m/d')}}</div>
+                                        <div class="card-panel red lighten-2" style="padding: 5px; color: #fff;">{{$buy->updated_at->format('d/m/Y')}}</div>
                                     @endif
                                 </td>
                                 <td>{{$buy->valor}} USD</td>
